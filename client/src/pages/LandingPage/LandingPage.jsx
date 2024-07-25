e686542198b5277283ad2e40222e14c5fcf64e9f
GabTorres17 <g.abotorres@hotmail.com>
2024-07-25 00:11:52 -0400

    landing page

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import styles from "../LandingPage/LandingPage.module.css";
+import satelliteImg from "../../assets/satellite.png";
 
 
 const LandingPage = () => {
@@ -16,9 +17,22 @@ const LandingPage = () => {
                 <h1>Bienvenidos a mi PI de Countries</h1>
                 <p>Disfruta tu estadía en mi proyecto individual, tengo muchos países de todas las formas y colores :D</p>
             </header>
-            <section className={styles.start}>
-                <button onClick={backToHome}>Comenzar</button>
-            </section>
+            <body>
+                <div className={styles.planetPosition}>
+                    <div className={styles.farOrbit}></div>
+                    <div className={styles.nearOrbit}></div>
+                    <div className={styles.mainPlanet}>
+                        <div className={styles.floatingObjects}>
+                            <div className={styles.satellite}>
+                                <img src={satelliteImg}></img>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <section className={styles.start}>
+                    <button onClick={backToHome}>Comenzar</button>
+                </section>
+            </body>
             <footer className={styles.footer}>
                 <p>&copy; 2024 Gabriel Torres Inc. All rights reserved.</p>
             </footer>
