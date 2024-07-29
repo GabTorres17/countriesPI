5a07535db1972eb97addc4c79df498d1c6a3e7f3
GabTorres17 <g.abotorres@hotmail.com>
2024-07-29 01:18:55 -0400

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'  // Asegúrate de que el directorio de salida esté configurado correctamente
-  },
-  server: {
-    port: 3000,  // El puerto en el que deseas ejecutar tu servidor de desarrollo
-  },
-});
+})
