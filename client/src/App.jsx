25035fde78ac50b2c57fb877c8e2be2de253f6f3
GabTorres17 <g.abotorres@hotmail.com>
2024-07-29 21:06:23 -0400

    deploy fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import LandingPage from './pages/LandingPage/LandingPage'
 import HomePage from './pages/HomePage/HomePage'
@@ -9,10 +9,6 @@ import NavBar from './components/NavBar/NavBar'
 import NotFound from './pages/NotFound/NotFound'
 
 function App() {
-  //const [count, setCount] = useState(0)
-  //<Route path="/countries" element={<HomePage />} >
-  //<Route path="/countries/:id" element={<DetailPage />} />
-
   return (
     <>
       <div className='w-full'>
